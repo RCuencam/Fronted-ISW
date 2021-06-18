85986f3cb40d4cc4072f7ac5e122b080bc6f1e7d
Rodrigo <rcuenca446@gmail.com>
2021-06-18 16:31:52 -0500

    fix(dev): Toolbar and SideMenu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostulationsComponent } from './pages/postulations/postulations.component';
 import {SearchJobsComponent} from "./pages/search-jobs/search-jobs.component";
 
 const routes: Routes = [
   {path:'jobs', component : SearchJobsComponent},
+  {path:'postulations', component: PostulationsComponent}
 ];
 
 @NgModule({
